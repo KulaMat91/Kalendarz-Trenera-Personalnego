4cf1473d049a0d2418614c0d83d3ce6a00a23245
JulitaJaros <julita.paulina.jaros@gmail.com>
2023-04-23 10:45:02 +0200

    add new vievs

---
+++ src/main/java/com/example/kalendarz_trenera_personalnego/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.kalendarz_trenera_personalnego.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+
+@Controller
+@RequiredArgsConstructor()
+public class HomeController {
+
+
+
+
+}
